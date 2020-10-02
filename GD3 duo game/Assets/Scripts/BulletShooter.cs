8cfcf50d13bccea9dbe861ff6388f001b2f9bfb4
stefan1234567891011 <s.hobeijn@hotmail.com>
2020-10-02 14:30:04 +0200

    tower shoots wall and bullet disappears when it hits wall

---
+++ GD3 duo game/Assets/Scripts/BulletShooter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletShooter : MonoBehaviour
+{
+    public float attackCooldown;
+    public GameObject bullet;
+
+    private float time;
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= attackCooldown)
+        {
+            GameObject oject = Instantiate(bullet);
+            oject.transform.position = transform.position;
+            oject.transform.parent = transform;
+            time = 0;
+        }
+    }
+}
---
+++ GD3 duo game/Assets/Scripts/DestroyOtherOnImpact.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOtherOnImpact : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("1");
+        if (collision.gameObject.tag == "Bullet")
+        {
+            Debug.Log("2");
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("7");
+    }
+}
