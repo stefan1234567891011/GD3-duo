66ece9576cf92fcdc5dd76558fdd02e07340383d
stefan1234567891011 <s.hobeijn@hotmail.com>
2020-10-08 14:56:49 +0200

    enemySpawner added

---
+++ GD3 duo game/Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    public GameObject Enemy;
+    public List<GameObject> spawnpoints;
+
+    private float time;
+    public float spawnCooldown;
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= spawnCooldown)
+        {
+            int rnd = Random.Range(0, spawnpoints.Count);
+            GameObject spawnpoint = spawnpoints[rnd];
+
+            GameObject temp = Instantiate(Enemy);
+            temp.transform.position = spawnpoint.transform.position;
+            temp.transform.parent = spawnpoint.transform;
+            time = 0;
+        }
+    }
+}
