068c9197f06914a150a2de54be310fea9c541321
brian <brian.vullers@home.nl>
2020-10-09 10:12:17 +0200

    add wall generation to map gen

--- GD3 duo game/Assets/Scripts/EnemyAttackHandler.cs
+++ GD3 duo game/Assets/Scripts/EnemyAttackHandler.cs
@@ -10,5 +10,6 @@ public class EnemyAttackHandler : MonoBehaviour
         {
             transform.position += new Vector3(3, 0);
         }
+        
     }
 }
--- GD3 duo game/Assets/Scripts/mapGenerate.cs
+++ GD3 duo game/Assets/Scripts/mapGenerate.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class mapGenerate : MonoBehaviour
 {
     public GameObject tilePrefab;
+    public GameObject wallPrefab;
     public SpawnEnemy spawn;
 
     public int mapWith = 3;
@@ -22,14 +23,27 @@ public class mapGenerate : MonoBehaviour
     {
         for (int x = 0; x < mapWith; x++)
         {
+            
             for (int y = 0; y < mapHeight; y++)
             {
-                GameObject  tempObj = Instantiate(tilePrefab);
-                tempObj.transform.position = new Vector3(transform.position.x + (x * offset), transform.position.y + (y * offset), 10);
-                tempObj.transform.parent = transform;
-                tempObj.name = x.ToString() + ", " + y.ToString();
+                
+                if(x==0)
+                {
+                    GameObject  tempWallObj = Instantiate(wallPrefab);
+                    tempWallObj.transform.position = new Vector3(transform.position.x + -2, transform.position.y  + (y * offset), 10);
+                    tempWallObj.transform.parent = transform;
+                    tempWallObj.name = x.ToString() + "left";
+                    GameObject  tempWallObj2 = Instantiate(wallPrefab);
+                    tempWallObj2.transform.position = new Vector3(transform.position.x + mapWith+2, transform.position.y  + (y * offset), 10);
+                    tempWallObj2.transform.parent = transform;
+                    tempWallObj2.name = x.ToString() + "right";
+                }
+                GameObject  temptileObj = Instantiate(tilePrefab);
+                temptileObj.transform.position = new Vector3(transform.position.x + (x * offset), transform.position.y + (y * offset), 10);
+                temptileObj.transform.parent = transform;
+                temptileObj.name = x.ToString() + ", " + y.ToString();
                 if(x == mapWith-1){
-                    spawn.spawnpoints.Add(tempObj);
+                    spawn.spawnpoints.Add(temptileObj);
                 }
             }
         }
