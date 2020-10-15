6ab50f1ab72db98ca4bf0abe67879085379f2bd9
brian <brian.vullers@home.nl>
2020-10-15 11:37:00 +0200

    first stage click and drag

---
+++ GD3 duo game/Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonClick : MonoBehaviour
+{
+    // Update is called once per frame
+    private Touch touch;
+    void Start() 
+    {
+        if (Input.touchCount>0)
+        {
+            touch = Input.GetTouch(0);
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            touchPosition.z = 0f;
+            transform.position = touchPosition;
+        }
+    }
+    void Update()
+    {
+        touch = Input.GetTouch(0);
+        if(touch.phase == TouchPhase.Moved){
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            touchPosition.z = 0f;
+            transform.position = touchPosition;
+        }
+        if(touch.phase == TouchPhase.Canceled || touch.phase == TouchPhase.Ended){
+            Destroy(gameObject);
+        }
+
+            
+    }
+
+}
