46341c6e91d99b996bc3a696a11c3b31b38884af
stefan1234567891011 <s.hobeijn@hotmail.com>
2020-10-15 14:27:52 +0200

    enemyspawner reworked
    - waves
    - every wave spawns an extra circle
    - every 3 waves an extra oval

--- GD3 duo game/Assets/Scripts/SpawnEnemy.cs
+++ GD3 duo game/Assets/Scripts/SpawnEnemy.cs
@@ -4,25 +4,96 @@ using UnityEngine;
 
 public class SpawnEnemy : MonoBehaviour
 {
-    public GameObject Enemy;
+    public GameObject enemy;
     public List<GameObject> spawnpoints;
 
-    private float time;
+    public float time;
     public float spawnCooldown;
+
+    public int wave = 1;
+    public int amountOfCircles = 1;
+    public int amountOfOvals = 0;
+    public float waveTime = 10;
+    public float breakTime = 5;
+    public bool waveBusy = true;
+
+    public int amountOfCirclesLeft;
+    public int amountOfOvalsLeft;
+    public float circleTime;
+    public float ovalTime;
+    public float circleTimer;
+    public float ovalTimer;
+
+    private void Start()
+    {
+        amountOfCirclesLeft = amountOfCircles;
+        amountOfOvalsLeft = amountOfOvals;
+        circleTime = (waveTime - 1) / amountOfCircles;
+        ovalTime = (waveTime - 1) / amountOfOvals;
+    }
+
     void Update()
     {
-        time += Time.deltaTime;
-        if (time >= spawnCooldown)
+        if (spawnpoints.Count >= 1)
         {
-            if(spawnpoints.Count <1)return;
+            time += Time.deltaTime;
+            circleTimer += Time.deltaTime;
+            ovalTimer += Time.deltaTime;
+
+            if (waveBusy)
+            {
+                if (circleTimer >= circleTime && amountOfCirclesLeft >= 0)
+                {
+                    SpawnUnit(enemy);
+                    amountOfCirclesLeft--;
+                    circleTimer = 0;
+                }
+                if (ovalTimer >= ovalTime && amountOfOvalsLeft >= 0)
+                {
+                    SpawnUnit(enemy);
+                    amountOfOvalsLeft--;
+                    ovalTimer = 0;
+                }
+                if (time >= waveTime)
+                {
+                    wave++;
+                    amountOfCircles++;
+                    if (wave % 3 == 0)
+                    {
+                        amountOfOvals++;
+                    }
+                    waveBusy = false;
+                    time = 0;
+                }
+            }
+            else
+            {
+                if (time >= breakTime)
+                {
+                    amountOfCirclesLeft = amountOfCircles;
+                    amountOfOvalsLeft = amountOfOvals;
+                    circleTime = (waveTime - 1) / amountOfCircles;
+                    ovalTime = (waveTime - 1) / amountOfOvals;
+                    waveBusy = true;
+                    time = 0;
+                    circleTimer = 0;
+                    ovalTimer = 0;
+                }
+            }
+        }
+    }
+
+    private void SpawnUnit(GameObject unit)
+    {
+        if (spawnpoints.Count >= 1)
+        {
             int rnd = Random.Range(0, spawnpoints.Count);
             GameObject spawnpoint = spawnpoints[rnd];
 
-            GameObject temp = Instantiate(Enemy);
-            temp.transform.position = new Vector3(spawnpoint.transform.position.x,spawnpoint.transform.position.y,spawnpoint.transform.position.z-1);
-           
+            GameObject temp = Instantiate(unit);
+            temp.transform.position = spawnpoint.transform.position + new Vector3(0, 0, -1);
+
             temp.transform.parent = transform;
-            time = 0;
         }
     }
 }
