c83612f6e42de4cc66b4df47870f56e55e081a18
stefan1234567891011 <s.hobeijn@hotmail.com>
2020-10-16 12:18:09 +0200

    added event triggered when enemy dies

--- GD3 duo game/Assets/Scripts/EnemyHealthHandler.cs
+++ GD3 duo game/Assets/Scripts/EnemyHealthHandler.cs
@@ -1,23 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EnemyHealthHandler : MonoBehaviour
 {
     public float health;
+    public UnityEvent OnCircleKilled = new UnityEvent();
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Bullet")
         {
-            health += -1;
+            health -= 1;
             if (health <= 0)
             {
+                OnCircleKilled.Invoke();
                 Destroy(gameObject);
             }
         }
         else if(collision.gameObject.tag == "Wall")
         {
+            OnCircleKilled.Invoke();
             Destroy(gameObject);
         }
     }
