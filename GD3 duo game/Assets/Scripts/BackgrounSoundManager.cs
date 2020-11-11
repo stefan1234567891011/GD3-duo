a1eb4698f25a6e889b493c8ef3c3e7504801d271
brian <brian.vullers@home.nl>
2020-11-11 20:39:59 +0100

    unity change sound

---
+++ GD3 duo game/Assets/Scripts/BackgrounSoundManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BackgrounSoundManager : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    private FMOD.Studio.EventInstance Instance;
+    public string fmodEvent;
+    void Start() 
+    {
+        Instance = FMODUnity.RuntimeManager.CreateInstance(fmodEvent);
+        Instance.start();
+    }
+    void Update()
+    {
+        Instance.setParameterByName("Intensetie", 100f);
+    }
+
+}
+
