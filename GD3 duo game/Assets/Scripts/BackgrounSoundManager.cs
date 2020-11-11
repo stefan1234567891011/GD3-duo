a3b5983e5af365b286bd9366b71d678136153609
brian <brian.vullers@home.nl>
2020-11-11 21:26:48 +0100

    change audio if amount of moster get more

--- GD3 duo game/Assets/Scripts/BackgrounSoundManager.cs
+++ GD3 duo game/Assets/Scripts/BackgrounSoundManager.cs
@@ -8,14 +8,44 @@ public class BackgrounSoundManager : MonoBehaviour
     [FMODUnity.EventRef]
     private FMOD.Studio.EventInstance Instance;
     public string fmodEvent;
+    public float LOW_MedTrigger = 10;
+    public float Med_HighTrigger = 20;
+    public List<GameObject> list;
+    private LaneCounter laneCounter;
     void Start() 
     {
         Instance = FMODUnity.RuntimeManager.CreateInstance(fmodEvent);
+
         Instance.start();
     }
     void Update()
     {
-        Instance.setParameterByName("Intensetie", 100f);
+        int amount = 0;
+        if(list.Count > 0)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                LaneRefrens laneRefrens = list[i].GetComponent<LaneRefrens>();
+
+                GameObject lane = laneRefrens.Lane;
+                laneCounter = lane.GetComponent<LaneCounter>();
+                amount += laneCounter.Counter;
+            }
+        }
+        
+        if (amount<LOW_MedTrigger)
+        {
+            Instance.setParameterByName("Intensetie", 20f);
+        }
+        else if( amount<Med_HighTrigger)
+        {
+            Instance.setParameterByName("Intensetie", 50f);
+        }
+        else
+        {
+            Instance.setParameterByName("Intensetie", 80f);
+        }
+        
     }
 
 }
